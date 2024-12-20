97e92076422ad05baf9e9d0dd61993555a69fd87
Hxxseung <soaorsith2@naver.com>
2024-12-20 13:43:49 +0900

    TestFile Move

--- src/test/java/com/example/locking/product/ProductServiceTest.java
+++ src/test/java/com/example/locking/product/ProductServiceTest.java
@@ -1,8 +1,5 @@
-package com.example.locking.item;
+package com.example.locking.product;
 
-import com.example.locking.product.Product;
-import com.example.locking.product.ProductRepository;
-import com.example.locking.product.ProductService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
